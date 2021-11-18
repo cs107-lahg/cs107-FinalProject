f290027ddcbf4f39076550d1b11b086426cba9c0
Geoffrey Liu <geoffreyliu@fas.harvard.edu>
2021-11-17 23:30:38 -0500

    add test_composite_functions.py

---
+++ tests/test_composite_functions.py
@@ -0,0 +1,13 @@
+import pytest
+import os
+import sys
+
+os.chdir(sys.path[0])
+sys.path.append('../')
+import lahg_ad as ad
+import numpy as np
+
+"""
+This file tests complex composite functions
+"""
+
