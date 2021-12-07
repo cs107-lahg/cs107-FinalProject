5a17f8bb615cf26ca5ec373fcdb51bd9efe868fd
fengling0410 <lingfeng@hsph.harvard.edu>
2021-12-07 00:39:38 -0500

    add test for Jacobian functions

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -18,17 +18,17 @@ def test_jacobian():
     x = ad.Variable(1, np.array([0, 1]))
     y = ad.Variable(2, np.array([1, 0]))
     f = [x**2, x+y, y**2]
-    assert np.array_equal(Jacobian_der(f), np.array([[0, 2], [1, 1], [4, 0]]))
-    assert np.array_equal(Jacobian_val(f), np.array([1, 3, 4]))
+    assert np.array_equal(ad.Jacobian_der(f), np.array([[0, 2], [1, 1], [4, 0]]))
+    assert np.array_equal(ad.Jacobian_val(f), np.array([1, 3, 4]))
     
     f = [x**2, 3, y**2]
     with pytest.raises(Exception):
-        Jacobian_der(f)
+        ad.Jacobian_der(f)
     
     z = ad.Variable(2, np.array([1, 0, 0]))
     f = [z**2, x+y, x*y]
     with pytest.raises(Exception):
-        Jacobian_der(f)
+        ad.Jacobian_der(f)
 
 if __name__ == '__main__':
     test_jacobian()
